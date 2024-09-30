2abdcae6223b1e7c142fdea921c5e9ae73b09529
kristiangalvan <kristian.galvan@gmail.com>
2024-09-30 17:21:55 -0500

    Committing initial files

---
+++ gamelibrary-backend/src/main/java/com/kristian/gamelibrary_backend/GamelibraryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kristian.gamelibrary_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamelibraryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamelibraryBackendApplication.class, args);
+	}
+
+}
